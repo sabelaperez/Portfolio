5475bbb7af3afaa7f362b75bdb1bf7cc3e5ae204
sabelapereez <sabelapereez@gmail.com>
2026-02-08 19:39:21 +0100

    minor-fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,11 @@ export default defineConfig({
   plugins: [react()],
   base:'/',
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild',
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react': ['react', 'react-dom'],
-          'i18n': ['react-i18next', 'i18next', 'i18next-browser-languagedetector', 'i18next-http-backend'],
-        }
-      }
-    }
+    outDir: 'dist'
   },
 
   server: {
     host: true,
     port: 3000,
-    open: false, // No abrir navegador automáticamente
-  },
-
-  preview: {
-    port: 4173,
-    host: true,
   },
 });
