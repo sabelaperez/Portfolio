8d0dfe5b98ef126a473dd4ac2067bc7c8a7d7e90
sabelapereez <sabelapereez@gmail.com>
2026-02-08 13:03:10 +0100

    fix: improve vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,30 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react': ['react', 'react-dom'],
+          'i18n': ['react-i18next', 'i18next', 'i18next-browser-languagedetector', 'i18next-http-backend'],
+        }
+      }
+    }
   },
+
   server: {
-    host: true, // ou host: '0.0.0.0'
-    port: 3000
-  }
+    host: true,
+    port: 3000,
+    open: false, // No abrir navegador automáticamente
+  },
+
+  preview: {
+    port: 4173,
+    host: true,
+  },
 });
